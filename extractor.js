65faa75205e132662134f84f6ded9da6a7dd7fd9
hawkrobe <rxdh@stanford.edu>
2016-03-04 11:12:31 -0800

    Modularize preprocessing step; add comments

--- extractor.js
+++ extractor.js
@@ -7,13 +7,22 @@ function topicl(classHandle, options) {
 
   // Build documents and clean-up text
   var documents = classInstanceArr.map(function(classInstance) {
-    var words = classInstance.innerHTML.split(/[ ,.–:;()]+/); // Pull out text from class instance
-    return words.map(function(word) {
-      var noHtmlWord = word.replace(/<\/?[^>]+(>|$)/g, ""); // Strip out html tags
-      var noPunctuationWord = noHtmlWord.replace(/[’\/{}=\_`~]/g,""); // and within-word
-      return noPunctuationWord.replace("&amp", "");
-    }).filter(Boolean); // Remove empty strings
-  });
 
+    // Pull out list of words from class instance
+    var words = classInstance.innerHTML.split(/[ ,.–:;()]+/);
+
+    // Strip words of html tags & punctuation, remove empty strings
+    return words.map(function(word) {
+      return stripWord(word);
+    }).filter(Boolean); 
+  });
+  
   return documents;
+};
+
+function stripWord(word) {
+  return word
+    .replace(/<\/?[^>]+(>|$)/g, "")  // Strip out html tags
+    .replace(/[’\/{}=\_`~]/g,"")  // Strip out within-word punctuation
+    .replace("&amp", ""); // Strip out weird ampersand thing
 };
